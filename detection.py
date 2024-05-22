d64adc07101b64533266b55cc6a014f72b1742a6
burak <byrkbrk@gmail.com>
2024-05-22 15:53:04 +0300

    update PromptOWLViT class

--- detection.py
+++ detection.py
@@ -9,13 +9,18 @@ from ultralytics import SAM
 
 class PromptOWLViT(object):
     def __init__(self, 
+                 image_name,
                  checkpoint_name="google/owlv2-base-patch16-ensemble",
                  task="zero-shot-object-detection"):
+        self.module_dir = os.path.dirname(__file__)
+        self.image_name = image_name
         self.detector = pipeline(model=checkpoint_name, task=task)
         self.sam = SAM("mobile_sam.pt")
     
-    def detect(self, image_path, labels, save=False):
-        image = self.resize_image(self.read_image(image_path))
+    def detect(self, labels, size=(1024, 1024), save=False):
+        """Detects bounding boxes for given image path and text labels"""
+        image = self.read_image(os.path.join(self.module_dir, "detection-images", self.image_name), 
+                                size=size)
         predictions = self.detector(image, labels)
         if save:
             self.plot_bboxes_on_image(image, predictions)
@@ -28,32 +33,41 @@ class PromptOWLViT(object):
         print(p)
 
     
-    def plot_bboxes_on_image(self, image, predictions):
+    def plot_bboxes_on_image(self, image, predictions, fpath=None):
         """Plots bboxes onto image and saves"""
         draw = ImageDraw.Draw(image)
         for prediction in predictions:
             draw.rectangle(list(prediction["box"].values()), outline="red", width=1)
             draw.text((prediction["box"]["xmin"], prediction["box"]["ymin"]),
                       f"{prediction['label']}: {round(prediction['score'], 2)}",
-                      fill="white")
-        image.save("boxes_on_image.png")
+                      fill="purple")
+        
+        if fpath is None:
+            fpath = os.path.join(self.module_dir, 
+                                 "detected-images", 
+                                 os.path.splitext(self.image_name)[0] + "_boxes_on_image.png")
+        image.save(fpath)
 
-    def read_image(self, image_path):
+    def read_image(self, image_path, size=None):
         """Reads image as Image object"""
-        return Image.open(image_path)
+        image = Image.open(image_path)
+        if size:
+            image = self.resize_image(image, size)
+        return image
     
-    def resize_image(self, image):
+    def resize_image(self, image, size):
+        """Resizes given image into specified size"""
         return transforms.Compose([
             transforms.ToTensor(), 
-            transforms.Resize((1024, 1024)), 
+            transforms.Resize(size), 
             transforms.ToPILImage()
             ])(image)
     
 
 
 if __name__ == "__main__":
-    prompt_owlvit = PromptOWLViT()
-    image_path = "female.jpg"
-    labels = ["hair", "eye"]
-    prompt_owlvit.detect(image_path, labels)
-    prompt_owlvit.segment(image_path, labels)
+    image_name = "decouple_1.jpeg"
+    #labels = ["hair", "eye", "head", "nose", "jacket"]
+    labels = ["jewellery"]
+    preds = PromptOWLViT(image_name).detect(labels, save=True)
+    print(preds)
