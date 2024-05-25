4e2c3f340eb49bc7fe4722c90fcec680024b2905
burak <byrkbrk@gmail.com>
2024-05-25 14:59:23 +0300

    update arg name

--- detect.py
+++ detect.py
@@ -5,10 +5,10 @@ from detection import PromptOWLViT
 
 def parse_arguments():
     """Returns parsed arguments"""
-    parser = ArgumentParser(description="Detects bounding boxes for given image and text prompt")
+    parser = ArgumentParser(description="Detects bounding boxes for given image and text prompts")
     parser.add_argument("image_name", type=str, default=None, 
                         help="Name of the image file that be processed. Note image file must be in 'segmentation-images' directory")
-    parser.add_argument("text_prompt", nargs="+", type=str, default=None, help="Text prompt for the model")
+    parser.add_argument("text_prompts", nargs="+", type=str, default=None, help="Text prompts for the model")
     parser.add_argument("--image_size", nargs="+", type=int, default=[1024, 1024],
                         help="Size (height, width) to which the image be transformed")
     return parser.parse_args()    
@@ -16,5 +16,5 @@ def parse_arguments():
 
 if __name__ == "__main__":
     args = parse_arguments()
-    print(args.text_prompt)
-    PromptOWLViT(args.image_name).detect(args.text_prompt, args.image_size, save=True)
+    print("Provided text prompts:", args.text_prompts)
+    PromptOWLViT(args.image_name).detect(args.text_prompts, args.image_size, save=True)
