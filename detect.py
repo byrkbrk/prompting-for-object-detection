b993682ae441eff4e656b786540b1a403b2ef399
burak <byrkbrk@gmail.com>
2024-06-11 03:08:55 +0300

    correct argument messages and set device as 'cpu'

--- detect.py
+++ detect.py
@@ -7,11 +7,11 @@ def parse_arguments():
     """Returns parsed arguments"""
     parser = ArgumentParser(description="Detects bounding boxes for given image and text prompts")
     parser.add_argument("image_name", type=str, default=None, 
-                        help="Name of the image file that be processed. Note image file must be in 'segmentation-images' directory")
+                        help="Name of the image file that be processed. Note image file must be in 'detection-images' directory")
     parser.add_argument("text_prompts", nargs="+", type=str, default=None, help="Text prompts for the model")
-    parser.add_argument("--image_size", nargs="+", type=int, default=[1024, 1024],
-                        help="Size (height, width) to which the image be transformed")
-    parser.add_argument("--device", type=str, default=None, help="Device that be used during inference")
+    parser.add_argument("--image_size", nargs="+", type=int, default=None,
+                        help="Size (width, height) to which the image be transformed. Default: None")
+    parser.add_argument("--device", type=str, default="cpu", help="Device that be used during inference. Default: 'cpu'")
     return parser.parse_args()    
 
 
